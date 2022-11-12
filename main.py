259a2bd82f63194b842d4f6322bf6915d9e58675
Rares Raulea <rares@graffino.com>
2022-11-12 16:35:50 +0200

    feat(centroids): display separating hyperplanes

--- main.py
+++ main.py
@@ -31,6 +31,20 @@ if len(xPoints) == 0:
     xPoints = list(map(lambda p: p.x, points))
     yPoints = list(map(lambda p: p.y, points))
 
+
+def draw_centroids():
+    cx_points = []
+    cy_points = []
+    for c in centroids:
+        cx_points.append(c.x)
+        cy_points.append(c.y)
+
+    config_plt()
+    for i in range(0, constants.dataSize):
+        plt.scatter(xPoints[i], yPoints[i], c=colors[i], s=20, linewidth=0)
+
+    plt.scatter(cx_points, cy_points, c='yellow')
+
 # generate cetroids and initialize them
 centroids = []
 n_of_centroids = generate_random_number_of_centroids()
@@ -103,6 +117,63 @@ while not new_E == E:
 
     print(new_E)
 
+all_points_x = []
+all_points_y = []
+colors2 = []
+for i in range(-300, 300):
+    for j in range(-300, 300):
+        all_points_x.append(i)
+        all_points_y.append(j)
+        current_point = Point(i, j)
+        similarityWith = {}
+        for final_centroid in centroids:
+            similarity = similarities.get_euclidian_distance_similarity(current_point, final_centroid)
+            similarityWith[final_centroid.index] = similarity
+        best_centroid_index = min(similarityWith, key=similarityWith.get)
+        best_centroid = next(x for x in centroids if x.index == best_centroid_index)
+        colors2.append(best_centroid.color)
+
+
+config_plt()
+for i in range(0, constants.dataSize):
+    plt.scatter(xPoints[i], yPoints[i], c=colors[i], s=20, linewidth=0)
+
+
+def get_neighbours(i, j):
+    result = []
+    if i == 0 or i == 599 or j == 0 or j == 599:
+        if i != 0:
+            result.append(600*(i-1) + j)
+        if j != 0:
+            result.append(600*i + j - 1)
+        if i != 599:
+            result.append(600*(i+1) + j)
+        if j != 599:
+            result.append(600*i + j + 1)
+    return result
+
+draw_centroids()
+
+for index, color in enumerate(colors2):
+    if index + 1 < len(colors2) and not color == colors2[index + 1]:
+        plt.scatter(all_points_x[index], all_points_y[index], c=color, s=20, linewidth=0)
+
+# for i in range(0, 599):
+#     for j in range(0, 599):
+#         current_index = i*600 + j
+#         print('CI: ', colors2[current_index])
+#         top_neighbour = (i-1)*600 + j
+#         left_neighbour = current_index - 1
+#         right_neighbour = current_index + 1
+#         bottom_neighbour = (i+1)*600 + j
+#         neighbours = get_neighbours(i, j)
+#         should_display = 1
+#         for n in neighbours:
+#             colors2[current_index] == colors2[n]
+#         if not colors2[top_neighbour] == colors2[current_index] and not colors2[left_neighbour] == colors2[current_index] and not colors2[bottom_neighbour] == colors2[current_index] and not colors2[right_neighbour] == colors2[current_index]:
+#             print('POINT')
+#             plt.scatter(all_points_x[current_index], all_points_y[current_index], c=colors2[current_index], s=20, linewidth=0)
+plt.show()
 
 # display_plot_with_data()
 # for c in centroids:
